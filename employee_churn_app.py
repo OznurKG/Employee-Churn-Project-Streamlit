bbe6a45dd808dc962e18ba5867ff5c69080258c6
OznurKG <101494438+OznurKG@users.noreply.github.com>
2022-11-06 13:58:42 +0300

    Update employee_churn_app.py

--- employee_churn_app.py
+++ employee_churn_app.py
@@ -12,8 +12,7 @@ st.set_page_config(page_title="Employee Churn App")
 #Import the dataset
 employee = pd.read_csv("HR_Dataset.csv")
 df = employee.copy()
-df["satisfaction_level"].astype(float)
-df["last_evaluation"].astype(float)
+
 #Clean columns
 df = df.iloc[:, [0,1,2,3,4,5,7,8,9,6]]
 df = df.rename(columns={"Departments " : "departments", "Work_accident" : "work_accident"})
@@ -122,11 +121,11 @@ else:
     def user_input_features():
 
         
-        satisfaction_level = st.sidebar.slider('Satisfaction Level', df["satisfaction_level"].min(), df["satisfaction_level"].max(), df["satisfaction_level"].median(), 0.01)
-        last_evaluation = st.sidebar.slider('Last Evaluation', df["last_evaluation"].min(), df["last_evaluation"].max(), df["last_evaluation"].median(), 0.01)
-        average_montly_hours = st.sidebar.number_input('Average Monthly Hours', df["average_montly_hours"].min(), df["average_montly_hours"].max(), int(df["average_montly_hours"].median()), 1)
-        number_project = st.sidebar.number_input('Number of Project', df["number_project"].min(), df["number_project"].max(), int(df["number_project"].median()), 1)
-        time_spend_company = st.sidebar.number_input('Time Spent in the Company', df["time_spend_company"].min(), df["time_spend_company"].max(), int(df["time_spend_company"].median()), 1)
+        satisfaction_level = st.sidebar.slider('Satisfaction Level', float(df["satisfaction_level"].min()), float(df["satisfaction_level"].max()), float(df["satisfaction_level"].median()), 0.01)
+        last_evaluation = st.sidebar.slider('Last Evaluation', float(df["last_evaluation"].min()), float(df["last_evaluation"].max()), float(df["last_evaluation"].median()), 0.01)
+        average_montly_hours = st.sidebar.number_input('Average Monthly Hours', int(df["average_montly_hours"].min()), int(df["average_montly_hours"].max()), int(df["average_montly_hours"].median()), 1)
+        number_project = st.sidebar.number_input('Number of Project', int(df["number_project"].min()), int(df["number_project"].max()), int(df["number_project"].median()), 1)
+        time_spend_company = st.sidebar.number_input('Time Spent in the Company', int(df["time_spend_company"].min()), int(df["time_spend_company"].max()), int(df["time_spend_company"].median()), 1)
         work_accident = st.sidebar.radio('Work Accident', ('Yes', 'No'))
         if work_accident == 'Yes':
             work_accident = 1
